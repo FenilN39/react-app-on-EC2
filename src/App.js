9ca0f3e3bfa4f11976dbe1e58a1b52359aab84c5
fenil <fenil@gmail.com>
2022-01-20 21:30:19 +0530

    final cheking

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.addition
+          Edit <code>src/App.js</code> and save to reload.addition making changies
         </p>
         <a
           className="App-link"
